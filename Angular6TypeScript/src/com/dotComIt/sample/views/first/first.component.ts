56cfad456ecdc7a1750bdb828e5020da7d60b268
jhouser <jeffry@dot-com-it.com>
2018-05-10 21:58:41 -0400

    Adding Build Scripts for Angular 6 + TypeScript

---
+++ Angular6TypeScript/src/com/dotComIt/sample/views/first/first.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by jhouser on 5/15/2017.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'first',
+    templateUrl : './com/dotComIt/sample/views/first/first.component.html',
+    styleUrls: [ './com/dotComIt/sample/views/first/first.component.css' ]
+})
+
+export class FirstComponent { }
